9b5adf80736342f67d0e46664ad08808c3502668
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-08-06 08:55:50 +0900

    feat: 오늘 날짜와 비교해 상영일이 이후인 영화를 찾는다

---
+++ movieRepository.ts
@@ -0,0 +1,21 @@
+import { createClient } from 'jsr:@supabase/supabase-js@2'
+import "jsr:@std/dotenv/load";
+
+const supabaseUrl = Deno.env.get('SUPABASE_URL');
+const supabaseKey = Deno.env.get('SUPABASE_ANON_KEY');
+
+const supabase = createClient(
+  supabaseUrl,
+  supabaseKey
+)
+
+export async function findByReleaseDateAfter(today: string) {
+  //날짜가 오늘 이후인 영화를 찾는다.
+  const { data, error } = await supabase
+    .from('upcoming_movie')
+    .select('title, release_date, poster_path, overview, id')
+    .gt('release_date', today)
+  return data
+}
+
+
