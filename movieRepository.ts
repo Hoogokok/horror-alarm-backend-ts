c2b3781a36a77a2ca878ab7d0d154221c1b5f364
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-08-06 11:22:51 +0900

    feat: supabase url과 key 값을 읽지 못하면 예외를 반환하게 수정함

--- movieRepository.ts
+++ movieRepository.ts
@@ -4,6 +4,10 @@ import "jsr:@std/dotenv/load";
 const supabaseUrl = Deno.env.get('SUPABASE_URL');
 const supabaseKey = Deno.env.get('SUPABASE_ANON_KEY');
 
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('SUPABASE_URL, SUPABASE_ANON_KEY 가 설정되지 않았습니다.');
+}
+
 const supabase = createClient(
   supabaseUrl,
   supabaseKey
