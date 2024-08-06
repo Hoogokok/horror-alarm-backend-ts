77949c461a9dcb6965486c21513fd7ba5916200b
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-08-06 19:15:01 +0900

    refactor: 넷플릭스 한국 호러 영화 정보를 찾는 함수의 반환값을 명시해주고, 값이 존재하지 않을 때 빈 배열을 반환하게 수정했다.

--- netflixRepository.ts
+++ netflixRepository.ts
@@ -1,6 +1,6 @@
 import { createClient } from 'jsr:@supabase/supabase-js@2'
 import "jsr:@std/dotenv/load";
-import { NetflixHorrorKrById } from "./netflixService.ts";
+import { NetflixHorrorKrById, NetflixHorrorKr } from "./netflixService.ts";
 
 const supabaseUrl = Deno.env.get('SUPABASE_URL');
 const supabaseKey = Deno.env.get('SUPABASE_ANON_KEY');
@@ -23,12 +23,17 @@ export async function findByExpiredDateAfter(today: string = new Date().toISOStr
     return data
 }
 
-export async function findNetflixHorrorKr(the_movie_db_ids: string[]) {
+export async function findNetflixHorrorKr(the_movie_db_ids: string[]): Promise<NetflixHorrorKr[]> {
     // the_movie_db_id가 있는 넷플릭스 공포 영화를 찾는다.
     const { data, error } = await supabase
         .from('netflix_horror_kr')
         .select('title, poster_path, id, the_movie_db_id')
         .in('the_movie_db_id', the_movie_db_ids)
+    
+    if (error || !data) {
+        return []
+    }
+
     return data
 }
 
