76c3367a66c1277cf8d53fcf24044b34d9330fa6
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-08-06 13:31:29 +0900

    feat: 날짜가 오늘과 같거나 이후인 넷플릭스 호러 영화 정보를 찾아 반환한다.

--- netflixRepository.ts
+++ netflixRepository.ts
@@ -5,10 +5,19 @@ const supabaseUrl = Deno.env.get('SUPABASE_URL');
 const supabaseKey = Deno.env.get('SUPABASE_ANON_KEY');
 
 if (!supabaseUrl || !supabaseKey) {
-  throw new Error('SUPABASE_URL, SUPABASE_ANON_KEY 가 설정되지 않았습니다.');
+    throw new Error('SUPABASE_URL, SUPABASE_ANON_KEY 가 설정되지 않았습니다.');
 }
 
 const supabase = createClient(
-  supabaseUrl,
-  supabaseKey
+    supabaseUrl,
+    supabaseKey
 )
+
+async function findByExpiredDateAfter(today: string = new Date().toISOString()) {
+    //날짜가 오늘 이후인 영화를 찾는다.
+    const { data, error } = await supabase
+        .from('netflix_horror_expiring')
+        .select('title, expired_date, the_movie_db_id')
+        .gte('expired_date', today)
+    return data
+}
