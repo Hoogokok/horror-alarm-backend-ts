25d94cf7683dfeeeea7bc3f5026f21f5a69da8f0
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-08-07 18:05:00 +0900

    feat: 상영 중 영화 요청이 들어오면 정보를 json으로 반환하는 함수를 작성했다.

--- horror-alarm-backend-ts/main.ts
+++ horror-alarm-backend-ts/main.ts
@@ -1,9 +1,12 @@
 import { Hono } from 'hono'
+import { getReleasedResponse, getUpcomingResponse } from './movieService.ts'
+import { prettyJSON } from 'hono/pretty-json'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get('/api/releasing', async (c) => {
+  return c.json(await getReleasedResponse())
+}
+)
 
 Deno.serve(app.fetch)
