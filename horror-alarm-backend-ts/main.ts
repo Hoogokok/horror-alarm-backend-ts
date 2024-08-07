79fe10a8a82ebe5d3d5302cdf8e1bb3a7424275d
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-08-07 18:27:51 +0900

    feat: 스트리밍 종료 예정 정보를 반환하는 요청 api 함수을 작성했다.

--- horror-alarm-backend-ts/main.ts
+++ horror-alarm-backend-ts/main.ts
@@ -1,5 +1,6 @@
 import { Hono } from 'hono'
 import { getReleasedResponse, getUpcomingResponse } from './movieService.ts'
+import { getExpiringResponse, getNetflixDetailResponse } from './netflixService.ts'
 import { prettyJSON } from 'hono/pretty-json'
 
 const app = new Hono()
@@ -10,6 +11,10 @@ app.get('/api/releasing', async (c) => {
 
 app.get('/api/upcoming', async (c) => {
   return c.json(await getUpcomingResponse())
+})
+
+app.get('/api/streaming/expired', async (c) => {
+  return c.json({ expiredMovies: await getExpiringResponse() })
 })
 
 Deno.serve(app.fetch)
