c215fdf3d90af2f4a8d671b818d91643f68565c9
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-09-17 09:25:32 +0900

    refactor: 스트리밍 서비스을 위한 페이지네이션 관련 쿼리 추가
    공급자 아이디에 따라 공포 영화 갯수를 세는 쿼리와 공급자 아이디와 페이지 수에 따라 스트리밍 영화를 반환하는  쿼리르 작성한다. 영화는 한 페이지 당 6개를 반환한다.

--- horror-alarm-backend-ts/streamingRepository.ts
+++ horror-alarm-backend-ts/streamingRepository.ts
@@ -1,19 +1,78 @@
 import { createClient } from 'jsr:@supabase/supabase-js@2'
 import "jsr:@std/dotenv/load";
 import { Provider, StreamingDetailResponse, StreamingHorrorExpiring, StreamingPageResponse } from "./streamingDatabseTypes.ts";
-
+import { sql } from "./db.ts";
 const supabaseUrl = Deno.env.get('SUPABASE_URL');
 const supabaseKey = Deno.env.get('SUPABASE_ANON_KEY');
-
+const itemPerPage = 6
 if (!supabaseUrl || !supabaseKey) {
     throw new Error('SUPABASE_URL, SUPABASE_ANON_KEY 가 설정되지 않았습니다.');
 }
+
 
 const supabase = createClient(
     supabaseUrl,
     supabaseKey
 )
 
+
+export async function countStreamingAllHorror(providerId: number): Promise<number> {
+    // 스트리밍 공포 영화의 수를 찾는다.
+    if (providerId !== 0) {
+        const result = await sql`
+        SELECT COUNT(*) FROM movie_providers WHERE the_provider_id = ${providerId}`
+        const totalPages = Math.ceil(result[0].count / itemPerPage)
+        return totalPages
+    }
+
+    const result = await sql`
+        SELECT COUNT(*) FROM movie_providers`
+    const totalPages = Math.ceil(result[0].count / itemPerPage)
+    return totalPages
+}
+
+export async function filterStreamingHorror(id: number, currentPage: number): Promise<Array<StreamingPageResponse>> {
+    // 스트리밍 공포 영화를 찾는다.
+    const offset = (currentPage - 1) * itemPerPage
+    if (id === 0) {
+        const result = await sql`
+        SELECT movie.title, movie.poster_path, movie.id AS movie_id, movie.release_date, movie.vote_average, movie.vote_count, movie_providers.the_provider_id        
+        FROM movie
+        JOIN movie_providers 
+        ON movie.id = movie_providers.movie_id
+        ORDER BY movie.release_date DESC   
+        LIMIT ${itemPerPage} OFFSET ${offset}`
+
+        return result.map((movie: any) => {
+            return {
+                title: movie.title,
+                posterPath: movie.poster_path,
+                id: movie.movie_id,
+                releaseDate: movie.release_date,
+                providers: movie.the_provider_id === "1" ? "넷플릭스" : "디즈니플러스"
+            }
+        })
+    }
+
+    const result = await sql`
+        SELECT movie.title, movie.poster_path, movie.id AS movie_id, movie.release_date, movie.vote_average, movie.vote_count, movie_providers.the_provider_id        
+        FROM movie
+        JOIN movie_providers 
+        ON movie.id = movie_providers.movie_id
+        WHERE movie_providers.the_provider_id = ${id}
+        ORDER BY movie.release_date DESC   
+        LIMIT ${itemPerPage} OFFSET ${offset}`
+
+    return result.map((movie: any) => {
+        return {
+            title: movie.title,
+            posterPath: movie.poster_path,
+            id: movie.movie_id,
+            releaseDate: movie.release_date,
+            providers: movie.the_provider_id === "1" ? "넷플릭스" : "디즈니플러스"
+        }
+    })
+}
 
 
 export async function findByExpiredDateAfter(today: string = new Date().toISOString()) {
@@ -29,7 +88,7 @@ export async function findStreamingHorror(the_movie_db_ids: string[]): Promise<S
     // the_movie_db_id가 있는 스트리밍 공포 영화를 찾는다.
     const { data, error } = await supabase
         .from('movie')
-        .select('title, poster_path, id, the_movie_db_id')
+        .select('title, poster_path, id, vote_average, vote_count, the_movie_db_id')
         .in('the_movie_db_id', the_movie_db_ids)
     if (error || !data) {
         return []
@@ -42,7 +101,7 @@ export async function findStreamingHorrorKrById(id: string): Promise<StreamingDe
     // 아이디로 공포 영화를 찾는다
     const { data, error } = await supabase
         .from('movie')
-        .select('title, poster_path, id, overview, release_date')
+        .select('title, poster_path, id, overview, release_date, vote_average, vote_count')
         .eq('id', id)
 
     const result = await getMovieProviderByMovieId(id)
@@ -53,7 +112,9 @@ export async function findStreamingHorrorKrById(id: string): Promise<StreamingDe
             id: "Unknown",
             overview: "Unknown",
             releaseDate: "Unknown",
-            providers: []
+            providers: [],
+            voteAverage: "Unknown",
+            voteCount: "Unknown"
         }
     }
 
@@ -67,19 +128,23 @@ export async function findStreamingHorrorKrById(id: string): Promise<StreamingDe
         id: data[0].id,
         overview: data[0].overview,
         releaseDate: data[0].release_date,
-        providers: providers
+        providers: providers,
+        voteAverage: data[0].vote_average,
+        voteCount: data[0].vote_count
     }
 }
 
-export async function findStremingHorrorPage(the_provider_id: string): Promise<StreamingPageResponse[]> {
+export async function findStremingHorrorPage(the_provider_id: string, page: number): Promise<StreamingPageResponse[]> {
     // 스트리밍 공포 영화를 11개까지 찾는다.
     const ids = await getMovieProviders(the_provider_id)
+    const start = page
+    const end = start + 10
 
     const { data, error } = await supabase
         .from('movie')
         .select('title, poster_path, id')
         .in('id', ids)
-        .range(0, 10)
+        .range(start, end)
 
     if (error || !data) {
         return []
